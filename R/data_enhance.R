de5880f688ba960358d562c0cdb4b899bf62aff4
Eason Wang <75122985+Innovation888K@users.noreply.github.com>
2026-05-06 19:57:04 +0800

    Update data_enhance.R

--- R/data_enhance.R
+++ R/data_enhance.R
@@ -1,46 +1,51 @@
 library(mzR)
-library(MSnbase)
-library(ProtGenerics)
 args <- commandArgs(trailingOnly = TRUE)
-work_dir <- args[1] 
-output=args[2] 
-enhance_num=args[3]
-dir.create(output, showWarnings = FALSE)
-for (f in list.files(work_dir)) {
-  file_name=paste0(work_dir,"\\",f)
+work_dir <- args[1]
+output <- args[2]
+enhance_num <- as.integer(args[3])
+dir.create(output, showWarnings = FALSE, recursive = TRUE)
+set.seed(42)
+files <- list.files(
+  work_dir,
+  pattern = "\\.(mzXML|mzML|cdf|CDF)$",
+  full.names = TRUE
+)
+for (file_name in files) {
+  message("Processing: ", file_name)
   file1 <- mzR::openMSfile(file_name)
-  file2 <- MSnbase::readMSData(
-    files = file_name,
-    msLevel. = c(1,2),
-    mode = "onDisk",
-    verbose = TRUE
-  )
   header <- mzR::header(file1)
-  set.seed(42)
   peaks <- ProtGenerics::peaks(object = file1)
-  rt <- ProtGenerics::rtime(object = file2)
-  rt_frame=data.frame(rt=rt,idx=1:length(rt))
-  rt_diff=rnorm(enhance_num,0,10)
-  rt_diff[rt_diff<0-rt[1]]=-rt[1]
-  peaks1=lapply(1:enhance_num,function(m) return(lapply(peaks,function(x){
-    x=data.frame(x)
-    mz_shift=rnorm(nrow(x),0,5)*ifelse(x$mz<400,400,x$mz)/1000000#5% shift>10ppm
-    x$mz=x$mz+mz_shift
-    int_shift=rnorm(nrow(x),0,0.06)#2.5% shift>10%
-    x$intensity=x$intensity*(1-int_shift)
-    return(as.matrix(x))
-  })))
-  for (i in 1:enhance_num) {
-    header$retentionTime <- rt_diff[i]
-    output_file <- paste0(output,"\\",i,'-',f)
-    print(output_file)
+  rt <- header$retentionTime
+  rt_diff <- rnorm(enhance_num, mean = 0, sd = 10)
+  for (i in seq_len(enhance_num)) {
+    augmented_peaks <- lapply(peaks, function(x) {
+      x <- data.frame(x)
+      if (nrow(x) == 0) {
+        return(as.matrix(x))
+      }
+      mz_shift <- rnorm(
+        nrow(x),
+        mean = 0,
+        sd = 5
+      ) * ifelse(x$mz < 400, 400, x$mz) / 1000000
+      x$mz <- x$mz + mz_shift
+      int_shift <- rnorm(nrow(x), mean = 0, sd = 0.06)
+      int_shift <- pmax(pmin(int_shift, 0.3), -0.3)
+      x$intensity <- x$intensity * (1 - int_shift)
+      x$intensity[x$intensity < 0] <- 0
+      return(as.matrix(x))
+    })
+    header_i <- header
+    header_i$retentionTime <- rt + rt_diff[i]
+    header_i$retentionTime[header_i$retentionTime < 0] <- 0
+    sample_name <- basename(file_name)
+    output_file <- file.path(output, paste0(i, "-", sample_name))
+    message("Writing: ", output_file)
     mzR::writeMSData(
-      object = peaks1[[i]],
+      object = augmented_peaks,
       file = output_file,
-      header = mzR::header(file1)
+      header = header_i
     )
   }
   mzR::close(file1)
 }
-
-
